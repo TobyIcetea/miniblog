09e807ff70fdf604e567fafbf71a224eb71f6c78
TobyIcetea <x2406862525@gmail.com>
2025-10-31 15:03:33 +0800

    feat(store): 实现store层基础结构及user/post模块
    添加store层基础代码结构，包括数据存储接口定义和实现，以及user和post模块的CRUD操作

---
+++ internal/apiserver/store/logger.go
@@ -0,0 +1,23 @@
+// Copyright 2025 TobyIcetea <x2406862525@163.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/onexstack/miniblog. The professional
+// version of this repository is https://github.com/onexstack/onex.
+
+package store
+
+import "github.com/TobyIcetea/miniblog/internal/pkg/log"
+
+// Logger is a logger that implements the logger interface.
+// Is uses the log package to log error messages with additional context.
+type Logger struct{}
+
+// NewLogger creates and returns a new instance of Logger.
+func NewLogger() *Logger {
+	return &Logger{}
+}
+
+// Error logs an error message with the provided context using the log package.
+func (l *Logger) Error(err error, msg string, kvs ...any) {
+	log.Errorw(msg, append(kvs, "err", err)...)
+}
